c105d7f62bfb22a62b96989b1e2854cf8884f05b
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-16 15:14:11 -0700

    added auth function

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,6 +8,28 @@ const secret = process.env.SECRET;
 const expiration = "2h";
 
 module.exports = {
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // We split the token string into an array and return actual token
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+    return req;
+  },
   signToken: function ({ email, username, _id }) {
     const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
