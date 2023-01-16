23fbff89aaeeda7175f6ce3ba7687220b2467f2e
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-16 13:51:48 -0700

    added script to seed db

---
+++ server/seeders/postSeeds.js
@@ -0,0 +1,19 @@
+module.exports = {
+  posts: [
+    {
+      postTitle: "bitchinApp",
+      postText: "I was using this cool and its totally really cool!",
+      username: "slimshady",
+    },
+    {
+      postTitle: "my dream",
+      postText: "I was dreaming at it was a great dream!",
+      username: "reallycoo",
+    },
+    {
+      postTitle: "Gaming",
+      postText: "I like playing games!",
+      username: "anotherUser",
+    },
+  ],
+};
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,2 +1,49 @@
 const db = require("../config/connection");
-const {} = require("../models");
+const { User, Post } = require("../models/");
+const { post } = require("../models/Comment");
+const { posts } = require("./postSeeds");
+const { users } = require("./userSeeds");
+
+const postsBulkCreate = async () => {
+  const response = await Post.insertMany(posts);
+};
+
+const userBulkCreate = async () => {
+  const response = await User.insertMany(users);
+};
+// userBulkCreate();
+const deleteDB = async () => {
+  const postResponse = await Post.deleteMany({});
+  const userResponse = await User.deleteMany({});
+  console.log(postResponse, userResponse);
+};
+
+const start = async () => {
+  try {
+    //delete
+    await deleteDB();
+    await postsBulkCreate();
+
+    //post find and array creation
+    const posts = await Post.find({});
+    const postIdArr = posts.map((element) => element._id);
+    // adding posts id to user posts array
+    users.forEach((element) => {
+      element.posts.push(postIdArr[0]);
+    });
+    await userBulkCreate(users);
+
+    const response = await User.find({}).populate("posts");
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// in order to add the posts to the user model
+// we need to break down the array
+// get post from the db with id
+//insert post into user post array
+// insert Users into DB
