6f2499490f030be378e3403da3f7447c4d88ee96
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-16 12:20:28 -0700

    added auth to graphQL context

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -12,6 +13,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
