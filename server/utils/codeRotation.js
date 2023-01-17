6032c9e85b3c33682215c5991f5a362564b1a2cb
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-17 16:08:19 -0700

    added rotations.js

---
+++ server/utils/codeRotation.js
@@ -0,0 +1,7 @@
+module.exports = {
+  async codeRotation() {
+    //get the date and check if there is a change,
+    //store the current day inside of local storage and on component load of the codewars component check the current day is === to the date it storage
+    //if theres a change display a new challenge by talking to the backend and grabbing the storages array of codeword objects
+  },
+};
