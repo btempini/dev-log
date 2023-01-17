a1f20690d4ebac1a68d57f64723058fc456a917a
max <maximiliangibes@gmail.com>
2023-01-17 14:06:26 -0700

    adds boilerplate to userprofile.js components

--- client/src/components/userprofile.js
+++ client/src/components/userprofile.js
@@ -1,2 +1,19 @@
-// import React from 'react
-// import './styles/userprofile.css'
+import React from "react";
+import "./styles/userprofile.css";
+
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div>
+      <img />
+      <section>
+        <h2></h2>
+        <h2></h2>
+        <h2></h2>
+      </section>
+    </div>
+  );
+}
+
+export default UserProfile;
