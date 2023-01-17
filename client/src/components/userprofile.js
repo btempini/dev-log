64032016088370c29a3c3bb4502d80370a3ec1ec
max <maximiliangibes@gmail.com>
2023-01-17 15:06:00 -0700

    getting ready to pull

--- client/src/components/userprofile.js
+++ client/src/components/userprofile.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./styles/userprofile.css";
-
-import React from "react";
-import User from "../../../server/models/User";
-import Post from "../../../server/models/Post";
+// import User from "../../../server/models/User";
+// import Post from "../../../server/models/Post";
+import { QUERY_SINGLE_USER } from "../utils/queries";
 
 function UserProfile() {
+  console.log("our query:", QUERY_SINGLE_USER);
   return (
     <div key={User._id}>
-      <img />
+      {/* <img /> */}
       {/* name level and github */}
       <section>
         <h2>{User.username}</h2>
@@ -22,9 +22,9 @@ function UserProfile() {
       <btn>add a post</btn>
       {/* user post */}
       <section>
-        <h3>{Post.postTitle}</h3>
+        <h3>{User.Post.postTitle}</h3>
         <h3>{Post.postedAt}</h3>
-        <img src={Post.image} />
+        {/* <img src={Post.image} /> */}
         <p>{Post.postText}</p>
       </section>
       {/* Friends list */}
