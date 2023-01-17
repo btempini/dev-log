9aa470f2e35788025e531dce7bc4cb97f8e4b8a5
Bryan Tempini <mistatempini@gmail.com>
2023-01-17 16:40:51 -0500

    built signup page

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,26 +1,30 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import './styles/form.css'
+import "./styles/form.css";
 const Form = () => {
   return (
-    <form>
-      <input placeholder="Username" name="username" />
-      <input placeholder="Full Name" name="fullName" />
-      <input placeholder="Password" name="password" />
-      <input placeholder="Re-enter Password" name="reEnterPassword" />
-      <input placeholder="Email" name="email" />
-      <select>
-        <option selected="selected" name="level">
-          n00b
-        </option>
-        <option>Junior Dev</option>
-        <option>Master Dev</option>
-        <option>Ben</option>
-      </select>
-      <input placeholder="Github URL" name="githubUrl" />
-      {/* Link button to Feed page */}
-      <button>Signup</button>
-    </form>
+    <div className="signupContainer">
+      <div className="leftHero"></div>
+      <div className="rightForm">
+        <form className="signupForm">
+          <h1 className="signupHeader">Please enter your info...</h1>
+          <input placeholder="Username" name="username" />
+          <input placeholder="Full Name" name="fullName" />
+          <input placeholder="Password" name="password" />
+          <input placeholder="Re-enter Password" name="reEnterPassword" />
+          <input placeholder="Email" name="email" />
+          <select name="level">
+            <option selected="selected">n00b</option>
+            <option>Junior Dev</option>
+            <option>Master Dev</option>
+            <option>Ben</option>
+          </select>
+          <input placeholder="Github URL" name="githubUrl" />
+          {/* Link button to Feed page */}
+          <button className="signupPageButton">Signup</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
