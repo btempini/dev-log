1354fb7980db80e1a32a86a1ffc08a3e17b200f3
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-16 17:14:19 -0700

    add front end queries for users, single user, and query me

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,123 @@
 //queries
+import { gql } from "@apollo/client";
+
+export const QUERY_USERS = gql`
+  query alluser {
+    users {
+      _id
+      username
+      fullName
+      email
+      password
+      posts {
+        _id
+        postTitle
+        postText
+        image
+        postedAt
+        username
+        likes
+        comments {
+          commentId
+          CommentText
+          username
+          likes
+          postedAt
+        }
+        commentCount
+      }
+      friends {
+        _id
+        username
+        fullName
+        bio
+        DevLvl
+        github
+      }
+      bio
+      DevLvl
+      github
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = `
+query singleProfile($userId: ID!) {
+    user(userId: $userId) {
+      DevLvl
+      _id
+      bio
+      email
+      fullName
+      github
+      password
+      username
+      posts {
+        _id
+        commentCount
+        comments {
+          commentId
+          CommentText
+          likes
+          postedAt
+          username
+        }
+        image
+        likes
+        postText
+        postTitle
+        postedAt
+        username
+      }
+      friends {
+        DevLvl
+        _id
+        bio
+        email
+        fullName
+        github
+        username
+      }
+    }
+  }
+`;
+export const QUERY_ME = gql`
+  query me {
+    me {
+      DevLvl
+      _id
+      bio
+      email
+      fullName
+      github
+      password
+      username
+      posts {
+        _id
+        commentCount
+        comments {
+          commentId
+          CommentText
+          likes
+          postedAt
+          username
+        }
+        image
+        likes
+        postText
+        postTitle
+        postedAt
+        username
+      }
+      friends {
+        DevLvl
+        _id
+        bio
+        email
+        fullName
+        github
+        username
+      }
+    }
+  }
+`;
