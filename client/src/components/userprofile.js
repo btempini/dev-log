025ba0ebedcca56ffecd0ead8b3814f3e93937eb
max <maximiliangibes@gmail.com>
2023-01-17 14:17:07 -0700

    adds more destructuring to the userprofile.js component

--- client/src/components/userprofile.js
+++ client/src/components/userprofile.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./styles/userprofile.css";
 
 import React from "react";
+import User from "../../../server/models/User";
 
 function UserProfile() {
   return (
-    <div>
+    <div key= {User._id}>
       <img />
+      {/* name level and github */}
       <section>
-        <h2></h2>
-        <h2></h2>
-        <h2></h2>
+        <h2>{User.username}</h2>
+        <h2>{User.DevLvl}</h2>
+        <h2>{User.github}</h2>
+      </section>
+      {/* Bio */}
+      <section>
+        <p>{User.bio}</p>
+      </section>
+      <btn>add a post</btn>
+      {/* user post */}
+      <section>
+        
       </section>
     </div>
   );
