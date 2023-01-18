857a01e7b0c0e0dee1e24e00aadc7bdd2d5c945d
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-01-18 13:37:33 -0700

    finished footer button styling, and link to homepage

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,19 +1,15 @@
-// the freakin github bro
-// on EVERY single page?
-
-// import React from "react";
-// import './styles/footer.css'
-
 import React from "react";
 import "./styles/footer.css";
-// import Github from "../assets/Github.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer2">
-      <btn className="devLog">
-        <span>{"<"}</span>dev.log<span>{">"}</span>
-      </btn>
+      <Link to="/">
+        <button className="devLogf">
+          <span>{"<"}</span>dev.log<span>{">"}</span>
+        </button>
+      </Link>
     </footer>
   );
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/header";
 import UserProfile from "../components/userprofile";
-import Footer from "../components/footer"
+import Footer from "../components/footer";
 
 const Profile = () => {
   return (
