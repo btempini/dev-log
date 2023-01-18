a1a3031907649da588f1275ce89a3cd9dcb8b360
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-01-18 11:12:08 -0700

    started styling Icon

--- client/src/components/homefooter.js
+++ client/src/components/homefooter.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './styles/homefooter.css'
+import Github from '../assets/icons/Github.png'
 
 function HomeFooter() {
     return(
-<footer>
-  <link type="image/png" sizes="32x32" rel="icon" href="../assets/icons/icons" />
+<footer className="footer">
+  <img src={Github} alt="Github icon" /> 
 </footer>
-
     );
 }
 
