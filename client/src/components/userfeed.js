6c7bcbb160a82bf25a584f350a55dec93cd8e95b
max <maximiliangibes@gmail.com>
2023-01-18 09:28:55 -0700

    adds structuring to userfeed.js component

--- client/src/components/userfeed.js
+++ client/src/components/userfeed.js
@@ -9,10 +9,32 @@ function userfeed() {
         <img />
         {/* profile button */}
         <btn></btn>
-
+          {/* choose if you want to see your friends feed or all posts feed */}
         <section>
             <btn>local scope</btn>
             <btn>global scope</btn>
+        </section>
+        {/* local scope feed */}
+        <section>
+          {/* Post Title */}
+          <h3></h3>
+          {/* Date the post was created */}
+          <h3></h3>
+          {/* image of the user */}
+          {/* <img></img> */}
+          {/* post body */}
+          <p></p>
+        </section>
+        {/* global scope feed */}
+        <section>
+          {/* Post Title */}
+          <h3></h3>
+          {/* Date the post was created */}
+          <h3></h3>
+          {/* image of the user */}
+          {/* <img></img> */}
+          {/* post body */}
+          <p></p>
         </section>
     </div>
   )
