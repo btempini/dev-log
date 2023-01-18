92a94840a83a1273d8791491db79efa83f0f8748
max <maximiliangibes@gmail.com>
2023-01-18 09:50:54 -0700

    commented out userprofile.js in components to interact with localhost.

--- client/src/components/userprofile.js
+++ client/src/components/userprofile.js
@@ -23,35 +23,35 @@ function UserProfile() {
 //   }
 //  }
   
-  return (
-    <div key={User._id}>
-      {/* <img src='' /> */}
-      {/* name level and github */}
-      <section>
-        <h2>{User.username}</h2>
-        <h2>{User.DevLvl}</h2>
-        <h2>{User.github}</h2>
-      </section>
-      {/* Bio */}
-      <section>
-        <p>{User.bio}</p>
-      </section>
-      <btn>add a post</btn>
-      {/* user post */}
-      <section>
-        <h3>{User.postTitle}</h3>
-        <h3>{Post.postedAt}</h3>
-        {/* <img src={Post.image} /> */}
-        <p>{Post.postText}</p>
-      </section>
-      {/* Friends list */}
-      <aside>
-        <h2>Friends</h2>
-        <ul></ul>
-        <h3>LUL NO FRIENDS</h3>
-      </aside>
-    </div>
-  );
+  // return (
+  //   <div key={User._id}>
+  //     {/* <img src='' /> */}
+  //     {/* name level and github */}
+  //     <section>
+  //       <h2>{User.username}</h2>
+  //       <h2>{User.DevLvl}</h2>
+  //       <h2>{User.github}</h2>
+  //     </section>
+  //     {/* Bio */}
+  //     <section>
+  //       <p>{User.bio}</p>
+  //     </section>
+  //     <btn>add a post</btn>
+  //     {/* user post */}
+  //     <section>
+  //       <h3>{User.postTitle}</h3>
+  //       <h3>{Post.postedAt}</h3>
+  //       {/* <img src={Post.image} /> */}
+  //       <p>{Post.postText}</p>
+  //     </section>
+  //     {/* Friends list */}
+  //     <aside>
+  //       <h2>Friends</h2>
+  //       <ul></ul>
+  //       <h3>LUL NO FRIENDS</h3>
+  //     </aside>
+  //   </div>
+  // );
 }
 
 export default UserProfile;
