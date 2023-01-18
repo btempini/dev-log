03e15cf4f8c2aa92d6084ac3cd376ec4175e7788
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2023-01-18 15:37:44 -0700

    added get for codewars

--- server/routes/dailyCodeWars.js
+++ server/routes/dailyCodeWars.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
+const axios = require("axios");
 
 router.get("/getDailyChallenge", async (req, res) => {
-  const response = await fetch(
-    "https://www.codewars.com/api/v1/code-challenges/{challenge}"
-  );
-  console.log(response);
+  //get from codewars API
+  try {
+    const data = await axios.get(
+      "https://www.codewars.com/api/v1/code-challenges/5831200eb812b8016d000094"
+    );
+    res.send(data.data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
