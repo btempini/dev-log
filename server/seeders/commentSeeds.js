79e4f767e1ea7df7fc3dafef1c9a08f2dc832f4d
max <maximiliangibes@gmail.com>
2023-01-20 13:13:34 -0700

    adds new seed data for commentSeeds.js

--- server/seeders/commentSeeds.js
+++ server/seeders/commentSeeds.js
@@ -11,4 +11,24 @@
     CommentText: "definetly more things!",
     username: "anotherUser",
   },
+  {
+    CommentText: "This is a great article!",
+    username: "slimshady",
+  },
+  {
+    CommentText: "I completely agree, this is very informative!",
+    username: "Michael-Jordan",
+  },
+  {
+    CommentText: "I've been trying to learn React and this article is very helpful!",
+    username: "Dwight Likes Bears",
+  },
+  {
+    CommentText: "Thanks for sharing your tips and tricks on Node.js, it's very useful!",
+    username: "Jackson",
+  },
+  {
+    CommentText: "I've been wondering about the difference between CSS Grid and Flexbox, thanks for explaining it!",
+    username: "Tessa",
+  },
 ];
