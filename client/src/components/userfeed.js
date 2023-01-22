b1a0fc1a84f051f564ceac65c4c4e5a376b465ed
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-01-22 10:39:52 -0700

    added necessary env file/info, added scroll to top button

--- client/src/components/userfeed.js
+++ client/src/components/userfeed.js
@@ -61,7 +61,7 @@ function UserFeed() {
             <p className="codeWarsBody">{codeWarsState.rank.name}</p>
             <p className="codeWarsTitles">URL:</p>
             <p className="codeWarsBody">
-              <a href={codeWarsState.url}>{codeWarsState.url}</a>
+              <a href={codeWarsState.url}>Click Here to try it out!!</a>
             </p>
           </div>
         </div>
@@ -74,6 +74,9 @@ function UserFeed() {
           </div>
           <div className="largerPost">
             <Post posts={posts} title="All posts" />
+            <button className="scroll">
+              <a href="#top">Scroll to top</a>
+            </button>
           </div>
         </div>
       </div>
--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -5,7 +5,7 @@ import Footer from "../components/footer"
 
 const Feed = () => {
   return (
-    <div>
+    <div id="top">
       <Header />
       <Userfeed />
       <Footer />
