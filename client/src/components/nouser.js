ad72b668ec317382bd5073406c9ac1e6af4bc66b
Tessa Elyse <tessa.elise.robinson@gmail.com>
2023-01-22 21:41:28 -0700

    Made styling changes to nouser

--- client/src/components/nouser.js
+++ client/src/components/nouser.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function NoUser() {
   return (
     <div className="userError">
-      <h1 className="noUser">No User Found</h1>
+      <h1 className="noUser">No User Found :{"("} </h1>
       <div className="returnBtn">
       <Link to="/feed">
         <button className="returnFeed">Return to feed</button>
