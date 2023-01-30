8e26a8ad3108fd3cb315ac6f068761d04f237612
Bryan Tempini <mistatempini@gmail.com>
2023-01-30 17:01:03 -0500

    updating for heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddPost from "./pages/AddPost";
 
 const httpLink = createHttpLink({
-  uri: `/graphql`,
+  uri: `graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
